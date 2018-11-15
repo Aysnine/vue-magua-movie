104a8d2dfbf16268c6824baa5b396a9fc034c7a0
CNine <cnine229@gmail.com>
2018-11-15 12:26:09 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  baseUrl: './',
+  productionSourceMap: false,
+  css: {
+      loaderOptions: {
+          // nib document http://stylus.github.io/nib/
+          stylus: {
+              use: [require('nib')()],
+              import: ['~nib/lib/nib/index.styl']
+          }
+      }
+  }
+}
