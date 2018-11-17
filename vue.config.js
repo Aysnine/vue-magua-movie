8a23f1f3a38486d1ff5ed89484fe3c5773f1ce57
CNine <cnine229@gmail.com>
2018-11-17 14:16:19 +0800

    修复图片引用问题

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,25 @@ module.exports = {
         import: ['~nib/lib/nib/index.styl']
       }
     }
+  },
+  chainWebpack: config => {
+    /* for compoennt assets of bootstrap-vue */
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options['transformAssetUrls'] = {
+          img: 'src',
+          image: 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'img-src',
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        }
+        return options
+      })
   }
 }
